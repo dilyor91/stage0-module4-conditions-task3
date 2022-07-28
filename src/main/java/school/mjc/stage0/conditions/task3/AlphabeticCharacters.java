62fb696e28173fd961adea96c64354dea67b19c6
dilyor91 <dilyor_05@mail.ru>
2022-07-28 10:57:07 +0500

    Task #3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character < 'A' || character >'z') {
+            System.out.println("wrong alphabet!");
+        } else if(character == 'a' || character == 'A'
+                ||character == 'e' || character == 'E'
+                ||character == 'i' || character == 'I'
+                ||character == 'o' || character == 'O'
+                ||character == 'u' || character == 'U'
+                ||character == 'y' || character == 'Y') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
