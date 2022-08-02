ad27f9114d6976382bb320428b298c2ad7454a28
dilyor91 <dilyor_05@mail.ru>
2022-08-02 12:49:27 +0500

    Task #3. Multiple Conditions v2

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if(salary < 0) {
-            System.out.println("wrong input");
+            System.out.println("wrong input!");
         } else if(salary <=10000) {
             System.out.println(salary*0.85);
         } else if(salary <= 20000) {
-            System.out.println(salary*0.72);
+            System.out.println(salary*0.82);
         } else if(salary > 20000) {
             System.out.println(salary*0.8);
         }
